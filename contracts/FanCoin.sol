cf8ab4da6749cff23ec8724d23d6ec869ff6b856
Daniel Rampelt <drampelt@gmail.com>
2017-09-16 15:57:57 -0400

    Add methods to check fan status, become a fan, and stop supporting a creator

--- contracts/FanCoin.sol
+++ contracts/FanCoin.sol
@@ -69,4 +69,59 @@ contract FanCoin is MintableToken {
 
     return msg.sender;
   }
+
+  function isFanOf(address _creator) constant returns (bool) {
+    for (uint i = 0; i < supporting[msg.sender].length; i++) {
+      if (supporting[msg.sender][i] == _creator) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function becomeFan(address _creator) returns (address) {
+    bool found = false;
+    for (uint i = 0; i < supporting[msg.sender].length; i++) {
+      if (supporting[msg.sender][i] == _creator) {
+        found = true;
+        break;
+      }
+    }
+    require(!found);
+
+    supporting[msg.sender].push(_creator);
+    fans[_creator].push(msg.sender);
+    return _creator;
+  }
+
+  function stopSupporting(address _creator) returns (address) {
+    uint index;
+    bool found = false;
+    uint256 length = supporting[msg.sender].length;
+    for (uint i = 0; i < length; i++) {
+      if (supporting[msg.sender][i] == _creator) {
+        index = i;
+        found = true;
+        break;
+      }
+    }
+    require(found && length > 0);
+    supporting[msg.sender][index] = supporting[msg.sender][length - 1];
+    delete supporting[msg.sender][length - 1];
+
+    found = false;
+    length = fans[_creator].length;
+    for (i = 0; i < length; i++) {
+      if (fans[_creator][i] == msg.sender) {
+        index = i;
+        found = true;
+        break;
+      }
+    }
+    require(found && fans[_creator].length > 0);
+    fans[_creator][index] = fans[_creator][length - 1];
+    delete fans[_creator][length - 1];
+
+    return _creator;
+  }
 }
