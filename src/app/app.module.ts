71f69e2fa42dca7a7eae45ca2cd68e9147cc19c9
Burak Icel <burakicel96@gmail.com>
2017-09-16 17:37:50 -0400

    Routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+const appRoutes: Routes = [
+  { path: 'profile', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ProfileComponent,
+    LandingPageComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,47 @@
+// https://angular.io/docs/ts/latest/guide/router.html
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+
+const appRoutes: Routes = [
+    {
+        path:"",
+        component: LandingPageComponent,
+    },
+    {
+        path:"profile",
+        component: ProfileComponent,
+    },
+    // {
+    //     path:"videos",
+    //     component: VideoListComponent,
+    // },
+    // {
+    //     path:"videos/:slug",
+    //     component: VideoDetailComponent,
+    // },
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
+
+
+
+
+
+
+
